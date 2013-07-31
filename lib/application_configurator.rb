d3df148a6c1d8a72196fcdfcb54bec0239898162
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-07-31 10:30:18 -0400

    Initial commit

---
+++ lib/application_configurator.rb
@@ -0,0 +1,5 @@
+require "application_configurator/version"
+
+module ApplicationConfigurator
+  # Your code goes here...
+end
---
+++ lib/application_configurator/version.rb
@@ -0,0 +1,3 @@
+module ApplicationConfigurator
+  VERSION = "0.0.1"
+end
